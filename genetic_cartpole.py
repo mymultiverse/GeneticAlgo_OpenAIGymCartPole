eb600e61b45d359df2eca694508637f114894f67
Yograj <yrajsm@gmail.com>
2018-01-03 18:14:55 +0900

    genetic_cartpole.py

--- genetic_cartpole.py
+++ genetic_cartpole.py
@@ -7,57 +7,81 @@ from statistics import median, mean
 
 
 env = gym.make('CartPole-v2')
-#action_set =[]
-award_set =[]
 
+award_set =[]
+nn_out =[]
 test_run = 15
 
+best_gen =[]
 
-def sig(x):
+
+def lreLu(x):
+	alpha=0.2
+	return tf.nn.relu(x)-alpha*tf.nn.relu(-x)
+
+def sigmoid(x):
 	return 1/(1+np.exp(-x))
 
 def reLu(x):
-	return np.maximum(0, x)
+	return np.maximum(0,x)
+
 
 def intial_gen(test_run):
 	input_weight = []
 	input_bias = []
+
 	hidden_weight = []
-	n_hid_node = 4
+	out_weight = [] #out
+
+	in_node = 4   # 4,2 combinatin good 
+	hid_node = 2
+
 	for i in range(test_run):
-		in_w = np.random.rand(4,n_hid_node)
+		in_w = np.random.rand(4,in_node)
 		input_weight.append(in_w)
 
-		in_b = np.random.rand(n_hid_node)
+		in_b = np.random.rand((in_node))
 		input_bias.append(in_b)
 
-		hi_w = np.random.rand(n_hid_node, 1)
-		hidden_weight.append(hi_w)
+		hid_w = np.random.rand(in_node,hid_node)
+		hidden_weight.append(hid_w)
+
+
+		out_w = np.random.rand(hid_node, 1)
+		out_weight.append(out_w)
 	
-	generation = [input_weight, input_bias, hidden_weight]
+	generation = [input_weight, input_bias, hidden_weight, out_weight]
 	return generation
 
-def nn(obs,in_w,in_b,hi_w):
+
+
+def nn(obs,in_w,in_b,hid_w,out_w):
+
 	obs = np.reshape(obs,(1,4))
-	  
-	A1 = reLu(np.dot(obs,in_w)+in_b.T)
+	#obs = obs/np.max(obs) 
 
-	hid_layer = np.dot(A1,hi_w)
-	out_put = reLu(hid_layer)
+	Ain = reLu(np.dot(obs,in_w)+in_b.T)
+	#hid_layer = np.dot(Ain,hid_w)
+	#Ahid = sigmoid(np.dot(Ain,hid_w))
+	Ahid = reLu(np.dot(Ain,hid_w))
 
-	if out_put < 0.99: #(this should be close to 1 for better results)
+	out_put = reLu(np.dot(Ahid,out_w))
+
+	nn_out.append(out_put)
+
+	if out_put  < 0.9002: #(this should be close to < 1(3,2)layers # <0.90001 4,2 layers
 		out_put = 0
 	else:
 		out_put = 1
 
 	return out_put
 
-def run_env(env,in_w,in_b,hi_w):
+def run_env(env,in_w,in_b,hid_w,out_w):
 	obs = env.reset()
 	award = 0
-	for t in range(5000):
-		#env.render() thia slows the process
-		action = nn(obs,in_w,in_b,hi_w)
+	for t in range(300):
+		#env.render() this slows the process
+		action = nn(obs,in_w,in_b,hid_w,out_w)
 		obs, reward, done, info = env.step(action)
 		award += reward 
 		if done:
@@ -72,8 +96,9 @@ def rand_run(env,test_run):
 	for episode in range(test_run):# run env 10 time
 		in_w  = generations[0][episode]
 		in_b = generations[1][episode]
-		hi_w =  generations[2][episode]
-		award = run_env(env,in_w,in_b,hi_w)
+		hid_w =  generations[2][episode]
+		out_w =  generations[3][episode]
+		award = run_env(env,in_w,in_b,hid_w,out_w)
 		award_set = np.append(award_set,award)
 	gen_award = [generations, award_set]
 	return gen_award  
@@ -82,7 +107,7 @@ def rand_run(env,test_run):
 def mutation(new_dna):
 
 	j = np.random.randint(0,len(new_dna))
-	if ( 0 <j < 7): # controlling rate of amount mutation
+	if ( 0 <j < 10): # controlling rate of amount mutation
 		for ix in range(j):
 			n = np.random.randint(0,len(new_dna)) #random postion for mutation
 			new_dna[n] = new_dna[n] + np.random.rand()
@@ -96,7 +121,7 @@ def crossover(Dna_list):
 	newDNA_list.append(Dna_list[0])
 	newDNA_list.append(Dna_list[1]) 
 	
-	for l in range(13):  # generation after crassover
+	for l in range(10):  # generation after crassover
 		j = np.random.randint(0,len(Dna_list[0]))
 		new_dna = np.append(Dna_list[0][:j], Dna_list[1][j:])
 
@@ -114,8 +139,13 @@ def reproduce(award_set, generations):
 
 	new_input_weight = []
 	new_input_bias = []
+
 	new_hidden_weight = []
+
+	new_output_weight =[]
+
 	new_award_set = []
+
 	for index in good_award_idx:
 		
 		w1 = generations[0][index]
@@ -124,12 +154,13 @@ def reproduce(award_set, generations):
 		b1 = generations[1][index]
 		dna_b1 = np.append(dna_in_w, b1)
 
-		wh = generations[2][index]
-		dna = np.append(dna_b1, wh)
+		w2 = generations[2][index]
+		dna_whid = w2.reshape(w2.shape[1],-1)
+		dna_w2 = np.append(dna_b1,dna_whid)
 
+		wh = generations[3][index]
+		dna = np.append(dna_w2, wh)
 
-		#wh = generations[1][index]
-		#dna = np.append(dna_in_w, wh) # single DNA
 
 		DNA_list.append(dna) # make 2 dna for good gerneration
 
@@ -137,21 +168,26 @@ def reproduce(award_set, generations):
 
 	for newdna in newDNA_list: # collection of weights from dna info
 		
-	 	newdna_in_w1 = np.array(newdna[:generations[0][0].size]) 
-	 	new_in_w = np.reshape(newdna_in_w1, (-1,generations[0][0].shape[1]))
-	 	new_input_weight.append(new_in_w)
+		newdna_in_w1 = np.array(newdna[:generations[0][0].size]) 
+		new_in_w = np.reshape(newdna_in_w1, (-1,generations[0][0].shape[1]))
+		new_input_weight.append(new_in_w)
 
 		new_in_b = np.array([newdna[newdna_in_w1.size:newdna_in_w1.size+generations[1][0].size]]).T #bias
-	 	new_input_bias.append(new_in_b)
+		new_input_bias.append(new_in_b)
 
-	 	sl = newdna_in_w1.size + new_in_b.size
-	 	new_hi_w   = np.array([newdna[sl:]]).T
-	 	new_hidden_weight.append(new_hi_w)
+		sh = newdna_in_w1.size + new_in_b.size
+		newdna_in_w2 = np.array([newdna[sh:sh+generations[2][0].size]])
+		new_hid_w = np.reshape(newdna_in_w2, (-1,generations[2][0].shape[1]))
+		new_hidden_weight.append(new_hid_w)
 
-	 	new_award = run_env(env, new_in_w, new_hi_w, new_in_b) #bias
-	 	new_award_set = np.append(new_award_set,new_award)
+		sl = newdna_in_w1.size + new_in_b.size + newdna_in_w2.size
+		new_out_w   = np.array([newdna[sl:]]).T
+		new_output_weight.append(new_out_w)
 
-	new_generation = [new_input_weight,new_input_bias,new_hidden_weight]
+		new_award = run_env(env, new_in_w, new_in_b, new_hid_w, new_out_w) #bias
+		new_award_set = np.append(new_award_set,new_award)
+
+	new_generation = [new_input_weight,new_input_bias,new_hidden_weight,new_output_weight]
 
 	return new_generation, new_award_set
 
@@ -159,16 +195,20 @@ def reproduce(award_set, generations):
 def evolution(env,test_run,n_of_generations):
 	gen_award = rand_run(env, test_run)
 
-	current_generations = gen_award[0] 
+	current_gens = gen_award[0] 
 	current_award_set = gen_award[1]
-	
+	best_gen =[]
 	A =[]
 	for n in range(n_of_generations):
-		new_generation, new_award_set = reproduce(current_award_set, current_generations)
-		current_gen = new_generation
+		new_generation, new_award_set = reproduce(current_award_set, current_gens)
+		current_gens = new_generation
 		current_award_set = new_award_set
+		avg = np.average(current_award_set)
+		if avg > 4500:
+			best_gen = np.array([current_gens[0][0],current_gens[1][0],current_gens[2][0],current_gens[3][0]])
+			np.save("newtest", best_gen)
 		a = np.amax(current_award_set)
-		print("generation: {}, score: {}".format(n, a))
+		print("generation: {}, score: {}".format(n+1, a))
 		A = np.append(A, a)
 	Best_award = np.amax(A)
 
@@ -182,6 +222,32 @@ def evolution(env,test_run,n_of_generations):
 	return plt.show()
 
 
-n_of_generations = 500
+n_of_generations = 10
 evolution(env, test_run, n_of_generations)
+
+
+param = np.load("newtest.npy")
+
+
+in_w = param[0]
+in_b = param[1]
+hid_w= param[2]
+out_w= param[3]
+
+
+def test_run_env(env,in_w,in_b,hid_w,out_w):
+	obs = env.reset()
+	award = 0
+	for t in range(5000):
+		env.render() #thia slows the process
+		action = nn(obs,in_w,in_b,hid_w,out_w)
+		obs, reward, done, info = env.step(action)
+		award += reward
+
+		print("time: {}, fitness: {}".format(t, award)) 
+		if done:
+			break
+	return award
+
+print test_run_env(env, in_w, in_b, hid_w,out_w)
 
